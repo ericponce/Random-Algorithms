27989368c52d28cf12692a89de9eb221399c760a
Eric Ponce <ericponce2004@yahoo.com>
2011-08-24 22:52:00 -0400

    Reduced Line Count (By 1, lol)

--- GCD.java
+++ GCD.java
@@ -1,15 +1,12 @@
-
 /**
  * Solves for the Greatest common devisor recursively and extremely fast.
  * Uses the Euclidean Algorith for GCD.
  * @author Eric Ponce
  */
 public class GCD {
-    
     public static int GCD(int i, int j) {
         if (i == 0) return j;
         else if (j == 0) return i;
-        else if (i > j) return GCD (j, i - j);
-        else return GCD(i, j - i);
+        else return (i > j) ? GCD(j, i - j) : GCD(i, j - i);
     }
 }
