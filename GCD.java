64d43f7ec6c29c7a959ef971b76954797152b605
Eric Ponce <ericponce2004@yahoo.com>
2011-09-07 23:05:32 -0400

    Shortened GCD Code and possibly (but not likely) increased speed

--- GCD.java
+++ GCD.java
@@ -1,12 +1,10 @@
 /**
  * Solves for the Greatest common devisor recursively and extremely fast.
- * Uses the Euclidean Algorithm for GCD.
  * @author Eric Ponce
  */
-public class GCD {
+public class GCD {    
     public static int GCD(int i, int j) {
-        if (i == 0) return j;
-        else if (j == 0) return i;
-        else return (i > j) ? GCD(j, i - j) : GCD(i, j - i);
+        if (j == 0) return i;
+        else return GCD(j, i%j);
     }
 }
